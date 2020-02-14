d4d0bfbd04069fc23ca082f0d1ee7687b098ea70
Jérémy <jeremy@code-rhapsodie.fr>
2020-02-14 14:32:31 +0100

    Added semi-colon after each query to ease copy-paste

--- src/Command/SchemaCommand.php
+++ src/Command/SchemaCommand.php
@@ -93,7 +93,7 @@ class SchemaCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $io->text('Execute these SQL Queries on your database:');
         foreach ($sqls as $sql) {
-            $io->text($sql);
+            $io->text($sql.';');
         }
     }
 }
